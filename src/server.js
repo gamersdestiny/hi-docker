93f5a09e5631d5fd28a97e7dcb6723198e901c1e
guna <guna57012@gmail.com>
2021-10-01 13:45:35 +0530

    initial compose test

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/views/index.html')
+})
+
+
+
+
+app.listen(3000, ()=> {
+    console.log('Server Started')
+})
